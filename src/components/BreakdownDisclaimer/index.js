ef12c2abffad0d6e20a5b8a2a52b0327d0f72d83
Saahil Khemnani <saahil.khemlani@gmail.com>
2023-01-20 18:10:15 -0800

    Accounting for "today" on breakdown disclaimer

--- src/components/BreakdownDisclaimer/index.js
+++ src/components/BreakdownDisclaimer/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Text, colors } from '../../style';
 import styles from './styles'
 import { disclaimer } from './constants';
+import moment from 'moment';
 
 export default BreakdownDisclaimer = ({date, progress}) => {
-	return (<Text body style={styles.disclaimer} color={colors.grey}>{disclaimer(date, progress)}</Text>)
+	let realDate = moment(date).isSame(moment(), 'day') ? 'today' : `on ${date}`
+	return (<Text body style={styles.disclaimer} color={colors.grey}>{disclaimer(realDate, progress)}</Text>)
 }
