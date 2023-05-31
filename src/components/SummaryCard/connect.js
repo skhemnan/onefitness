057f359a15cf0675d2bcb931a649bd967982124d
Saahil Khemnani <saahil.khemlani@gmail.com>
2023-05-31 20:22:46 +0700

    Handling when max is hit

--- src/components/SummaryCard/connect.js
+++ src/components/SummaryCard/connect.js
@@ -5,6 +5,7 @@ import firestore from '@react-native-firebase/firestore'
 import moment from 'moment';
 import { setExercises } from '../../redux/action/Workout';
 import { useSelector } from 'react-redux';
+import { Alert } from 'react-native';
 
 
 export const useConnect = ({item}) => {
@@ -24,28 +25,37 @@ export const useConnect = ({item}) => {
 
 	const markComplete = async () => {
 		try {
-			let curIndex = Object.keys(PROGRESSION_DATA).indexOf(`${navParams.summary.progression}`)
-			let nextProgress = Object.keys(PROGRESSION_DATA)[curIndex + 1]
-			let exercises = await store.getState().Workout.exercises
-			let found = exercises.findIndex(x => x.id == navParams.id)
-			let dateNum = moment(exercises[found].nextWorkoutDate).day()
-			let newDate = moment().add(1, 'week').isoWeekday(dateNum)
-
-			let updatedExercise = {
-				...exercises[found],
-				progress: nextProgress,
-				nextWorkoutDate: firestore.Timestamp.fromDate(moment(newDate).toDate())
+			// If max has been reached
+			if(navParams.summary.progression == 1){
+				Alert.alert('Congratulations!', "You've hit your max weight! Your new max will go up by 5lbs.", [
+					{ text: 'Awesome!', style: 'OK' }
+				])
 			}
 
-			let newExercises = exercises.filter(x => x.id != exercises[found].id)
-			newExercises.push(updatedExercise)
+				let curIndex = Object.keys(PROGRESSION_DATA).indexOf(`${navParams.summary.progression}`)
+				let nextProgress = Object.keys(PROGRESSION_DATA)[curIndex + 1]
+				let exercises = await store.getState().Workout.exercises
+				let found = exercises.findIndex(x => x.id == navParams.id)
+				let dateNum = moment(exercises[found].nextWorkoutDate).day()
+				let newDate = moment().add(1, 'week').isoWeekday(dateNum)
+				let newMax = navParams.summary.progression == 1 ? parseInt(exercises[found].max) + 5 : parseInt(exercises[found].max)
 
-			// Send updated exercise to firebase and update on redux	
-			await store.dispatch(setExercises(newExercises))
-			await firestore().collection('users').doc(uid).update({exercises: newExercises})
+				let updatedExercise = {
+					...exercises[found],
+					progress: nextProgress,
+					nextWorkoutDate: firestore.Timestamp.fromDate(moment(newDate).toDate()),
+					max: newMax.toString()
+				}
 
-			// console.log('CHANGING EXERCISE', exercises[found])
-			// console.log('TO', updatedExercise)	
+				let newExercises = exercises.filter(x => x.id != exercises[found].id)
+				newExercises.push(updatedExercise)
+
+				// Send updated exercise to firebase and update on redux	
+				await store.dispatch(setExercises(newExercises))
+				await firestore().collection('users').doc(uid).update({exercises: newExercises})
+
+				// console.log('CHANGING EXERCISE', exercises[found])
+				// console.log('TO', updatedExercise)	
 		} catch (error) {
 			console.log('MARK COMPLETE ERROR', error, error?.message)	
 		}
