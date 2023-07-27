9dfc34d1febc9777e0f57e4d367c56c112f645df
Saahil Khemnani <saahil.khemlani@gmail.com>
2023-07-27 18:25:10 +0700

    fix: keyboard unable to dismiss for input

--- src/screens/EditMax/index.js
+++ src/screens/EditMax/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { View, Text, colors, Button, Card, Input } from '../../style'
 import { Alert } from 'react-native';
 import styles from './styles'
@@ -9,19 +10,22 @@ const EditMax = ({navigation, route}) => {
 	const {handleConfirm, setCurrentMax} = useConnect({route, navigation})
 
 	return (
-		<View bg color={colors.darkGrey} style={styles.background}>
-			<View row color={colors.darkGrey} style={styles.header}>
-				<Button body onPress={() => navigation.goBack()} text="Cancel" textStyle={styles.headerText}/>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View bg color={colors.darkGrey} style={styles.background}>
+				<View row color={colors.darkGrey} style={styles.header}>
+					<Button body onPress={() => navigation.goBack()} text="Cancel" textStyle={styles.headerText}/>
+				</View>
+					<View center column style={styles.container}>
+						<Text h1 style={styles.text}>Enter your new max weight</Text>
+						<Card contact style={styles.input}>
+							<Input onChangeText={x => setCurrentMax(x)}/>
+						</Card>
+						<Text h4 style={styles.text}>Changing your max weight will reset your program.</Text>
+					</View>
+				<Button confirm text="CONFIRM" style={styles.button} onPress={handleConfirm}/>
 			</View>
-			<View center column style={styles.container}>
-				<Text h1 style={styles.text}>Enter your new max weight</Text>
-				<Card contact style={styles.input}>
-					<Input onChangeText={x => setCurrentMax(x)}/>
-				</Card>
-				<Text h4 style={styles.text}>Changing your max weight will reset your program.</Text>
-			</View>
-			<Button confirm text="CONFIRM" style={styles.button} onPress={handleConfirm}/>
-		</View>
+		</TouchableWithoutFeedback>
+
 	)
 }
 
--- src/screens/LogWeight/index.js
+++ src/screens/LogWeight/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { View, colors, Button, Text, Card, Input } from '../../style'
 import styles from './styles'
 import { useConnect } from './connect';
@@ -6,16 +7,18 @@ import { useConnect } from './connect';
 const LogWeight = ({navigation}) => {
 	const {setWeight, sendWeight} = useConnect({navigation})
 	return (
-		<View bg color={colors.darkGrey} style={styles.background}>
-			<View center column style={styles.container}>
-				<Text h1 style={styles.text}>Enter your new body weight (lbs)</Text>
-				<Card contact style={styles.input}>
-					<Input onChangeText={x => setWeight(x)}/>
-				</Card>
-				<Text h4 style={styles.text}>This will affect all your body weight exercises.</Text>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View bg color={colors.darkGrey} style={styles.background}>
+				<View center column style={styles.container}>
+					<Text h1 style={styles.text}>Enter your new body weight (lbs)</Text>
+					<Card contact style={styles.input}>
+						<Input onChangeText={x => setWeight(x)}/>
+					</Card>
+					<Text h4 style={styles.text}>This will affect all your body weight exercises.</Text>
+				</View>
+				<Button confirm text="SET NEW WEIGHT" style={styles.button} onPress={sendWeight}/>
 			</View>
-			<Button confirm text="SET NEW WEIGHT" style={styles.button} onPress={sendWeight}/>
-		</View>
+		</TouchableWithoutFeedback>
 	)
 }
 
